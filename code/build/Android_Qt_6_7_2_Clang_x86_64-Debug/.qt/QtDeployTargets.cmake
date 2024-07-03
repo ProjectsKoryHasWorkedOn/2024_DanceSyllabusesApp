00283f1c28dcfe04719018258eb201cf88ccce21
Kory <kory@LAPTOP-LCCT68U5.localdomain>
2024-07-03 21:25:51 +0930

    commit

---
+++ code/build/Android_Qt_6_7_2_Clang_x86_64-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_code_FILE C:/Users/Certa/Desktop/projects/dancesyllabuses_app/code/build/Android_Qt_6_7_2_Clang_x86_64-Debug/libcode_x86_64.so)
+set(__QT_DEPLOY_TARGET_code_TYPE MODULE_LIBRARY)
