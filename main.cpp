2b84ce2b3b0f69f79caebf48da60c084d8b142fa
Kory <kory@LAPTOP-LCCT68U5.localdomain>
2024-07-25 17:17:00 +0930

    not connecting to DB directly from app

--- main.cpp
+++ main.cpp
@@ -1,34 +1,10 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QSqlDatabase>
-#include <QtSql>
-#include <QSqlQuery>
 #include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
-    /*
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("az1-ts2.a2hosting.com");
-    db.setDatabaseName("krycom_dancers_db");
-    db.setUserName("krycom_kory");
-    db.setPassword("dJDdkFHDbExWu7WrOeSw");
-
-    // @todo: Make this work
-        // @todo: Address this "QSqlDatabase: QMYSQL driver not loaded"
-        // @todo: Communicate with web server from a API
-    if (db.open())
-    {
-        qDebug() << "Connected!";
-    }
-    else
-    {
-        qDebug() << "Failed to connect.";
-        return 0;
-    }
-    */
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/Dance_Syllabuses/import/qml/Main.qml"));
